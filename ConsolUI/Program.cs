086475ba189acf4c1aa2136c650767bcfda009ba
taner <taner@DESKTOP-8FBSMG9>
2021-07-22 22:06:16 +0300

    9 video

--- ConsolUI/Program.cs
+++ ConsolUI/Program.cs
@@ -9,12 +9,21 @@ namespace ConsolUI
     {
         static void Main(string[] args)
         {
+            // ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach(var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach(var item in productManager.GetByUnitPrice(40,100))
+            foreach (var item in productManager.GetByUnitPrice(40, 100))
             {
                 Console.WriteLine(item.ProductName);
             }
-            
         }
     }
 }
--- Core/DateAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DateAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,10 +1,66 @@
-﻿using System;
+﻿using Core.DataAccess;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Core.DateAccess.EntityFramework
 {
-  public  class EfEntityRepositoryBase<TEntity,TContext>
+    public class EfEntityRepositoryBase<TEntity, TContext>:IEntityRepository<TEntity>
+          where TEntity : class, IEntity, new()
+          where TContext : DbContext,new()
     {
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Detached;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> Filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return Filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(Filter).ToList();
+
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var UpdatedEntity = context.Entry(entity);
+                UpdatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
     }
 }
